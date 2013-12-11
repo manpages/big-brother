25246edb6b66b592da740efdbb345a2a12c87bf1
Jonn Mosotovy <amarr.industrial@gmail.com>
2013-12-11 23:51:13 +0100

    Fuck that shit.

--- refactor/big_brother/README.md
+++
@@ -1,3 +0,0 @@
-# BigBrother
-
-** TODO: Add description **
--- refactor/big_brother/lib/http.ex
+++
@@ -1,8 +0,0 @@
-defmodule BigBrother.HTTP do
-  def init(_, _, []) do
-    {:ok, req, nil}
-  end
-  
-  def handle(req, state) do
-  end
-end
